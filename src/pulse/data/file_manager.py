fa95e5e4cbc80309ab089cd341322e6fdd642588
karanikolopoulos <ckaranikolopoulos@gmail.com>
2025-11-09 23:29:58 +0000

    Refactor file system

--- src/pulse/data/file_manager.py
+++ src/pulse/data/file_manager.py
@@ -12,9 +12,10 @@ logger = logging.getLogger(__name__)
 
 class FileStatus(StrEnum):
     OK = "ok"
-    EXISTS = "File name already exists"
-    INVALID_SCHEMA = "Invalid schema (required fields: 'A', 'B', 'alias')."
-    MISSING_VALUES = "File contains missing values."
+    NOT_FOUND = "File not found"
+    DUPLICATE = "File name already exists"
+    INVALID_SCHEMA = "Invalid schema (required fields: 'A', 'B', 'alias')"
+    MISSING_VALUES = "File contains missing values"
 
 
 @dataclass
@@ -37,7 +38,7 @@ class PulseFile:
             "cols": len(self.df.columns),
         }
 
-        return json.dumps(obj=data, indent=2)
+        return json.dumps(obj=data, indent=4)
 
 
 class FileManager:
@@ -54,34 +55,21 @@ class FileManager:
             df = pd.read_json(file) if file.suffix == ".json" else pd.read_csv(file)
             self._add(file=file, df=df)
 
+    @property
+    def keys(self) -> list[str]:
+        return list(self.data.keys())
+
     def add(self, name: str, df: pd.DataFrame) -> FileStatus:
         file = self.root.joinpath(name).with_suffix(".json")
         status = self._add(file=file, df=df)
         if status == FileStatus.OK:
-            self[name].df.to_json(file, orient="records", indent=2)
+            self[name].df.to_json(file, orient="records", indent=4, index=False)
         return status
-
-    def delete(self, name: str):
-        if pulse_file := self.data[name]:
-            # remove from disk
-            pulse_file.path.unlink(missing_ok=True)
-            # update data
-            del self.data[name]
-
-    def update(self, name: str, df: pd.DataFrame):
-        if pulse_file := self.data[name]:
-            # update disk file
-            if pulse_file.suffix == ".json":
-                df.to_json(pulse_file.path)
-            else:
-                df.to_csv(pulse_file.path, index=False)
-            # update data
-            pulse_file.data = df
 
     def _add(self, file: Path, df: pd.DataFrame) -> FileStatus:
         if file.stem in self.data:
             logger.info(f"File {file.stem} already exists.")
-            return FileStatus.EXISTS
+            return FileStatus.DUPLICATE
 
         if not self._is_valid_schema(df=df):
             logger.info(f"File {file.stem} has invalid schema.")
@@ -96,6 +84,36 @@ class FileManager:
 
         return FileStatus.OK
 
+    def delete(self, name: str) -> FileStatus:
+        if pulse_file := self.data.get(name):
+            # remove from disk
+            pulse_file.path.unlink(missing_ok=True)
+            # update data
+            del self.data[name]
+            return FileStatus.OK
+        return FileStatus.NOT_FOUND
+
+    def update(self, name: str, df: pd.DataFrame) -> FileStatus:
+        if pulse_file := self.data.get(name):
+            # Validate the new dataframe
+            if not self._is_valid_schema(df=df):
+                logger.info(f"File {name} has invalid schema.")
+                return FileStatus.INVALID_SCHEMA
+
+            if self._has_nan(df=df):
+                logger.info(f"File {name} contains missing values.")
+                return FileStatus.MISSING_VALUES
+
+            # update disk file
+            if pulse_file.suffix == ".json":
+                df.to_json(pulse_file.path, orient="records", indent=4)
+            else:
+                df.to_csv(pulse_file.path, index=False)
+            # update data
+            pulse_file.df = df
+            return FileStatus.OK
+        return FileStatus.NOT_FOUND
+
     def _is_valid_schema(self, df: pd.DataFrame) -> bool:
         return self.schema.issubset(df.columns)
 
@@ -105,5 +123,5 @@ class FileManager:
     def __contains__(self, key: str) -> bool:
         return key in self.data
 
-    def __getitem__(self, key: str) -> PulseFile:
-        return self.data[key]
+    def __getitem__(self, key: str) -> PulseFile | None:
+        return self.data.get(key, None)
--- src/pulse/data/task_manager.py
+++ src/pulse/data/task_manager.py
@@ -1,5 +1,6 @@
 from copy import copy
 from enum import StrEnum
+from pathlib import Path
 
 from pulse.utils.paths import TASKS
 from pulse.data.pulse_task import PulseConfig
@@ -12,52 +13,50 @@ class TaskStatus(StrEnum):
 
 
 class TaskManager:
-    def __init__(self):
-        self.tasks: dict[str, PulseConfig] = self._read_tasks()
-
-    def _read_tasks(self):
-        tasks = {}
-
-        if not TASKS.exists() or not TASKS.is_dir():
-            return tasks
-
-        for yaml_file in list(TASKS.glob("*.yaml")):
-            task_config = PulseConfig.from_yaml(yaml_file)
-            tasks[task_config.task] = task_config
-
-        return tasks
-
-    def add(self, task_config: PulseConfig) -> TaskStatus:
-        # allow task overwrite
-        # if self._name_exists(name=task_config.task):
-        #     return TaskStatus.EXISTS
-
-        if self._is_duplicate(task_config=task_config):
-            return TaskStatus.DUPLICATE
-
-        task_config.save()
-        self.tasks[task_config.task] = copy(task_config)
-
-        return TaskStatus.OK
-
-    def delete(self, task_name: str) -> TaskStatus:
-        if not self._name_exists(name=task_name):
-            return TaskStatus.EXISTS
-
-        self.tasks.pop(task_name)
-        path = TASKS / f"{task_name}.yaml"
-        path.unlink(missing_ok=True)
-        return TaskStatus.OK
-
-    def _name_exists(self, name: str) -> bool:
-        return name in self
-
-    def _is_duplicate(self, task_config: PulseConfig) -> bool:
-        all_tasks = self.tasks.values()
-        return any(task_config == task for task in all_tasks)
+    def __init__(self, dir: Path):
+        self.tasks: dict[str, PulseConfig] = self._read_tasks(dir=dir)
 
     def __contains__(self, task: str) -> bool:
         return task in self.tasks
 
     def __getitem__(self, key: str) -> PulseConfig:
-        return self.tasks[key]
+        return copy(self.tasks[key])
+
+    def add(self, task_config: PulseConfig) -> TaskStatus:
+        if self._is_duplicate(task_config=task_config):
+            return TaskStatus.DUPLICATE
+
+        task_config.save()
+        self.tasks[task_config.name] = copy(task_config)
+
+        return TaskStatus.OK
+
+    def delete(self, task_name: str) -> TaskStatus:
+        if task_name not in self:
+            return TaskStatus.EXISTS
+
+        del self.tasks[task_name]
+        path = TASKS / f"{task_name}.yaml"
+        path.unlink(missing_ok=True)
+
+        return TaskStatus.OK
+
+    def _read_tasks(self, dir: Path):
+        tasks = {}
+
+        if not dir.exists() or not dir.is_dir():
+            return tasks
+
+        for yaml_file in list(dir.glob("*.yaml")):
+            task_config = PulseConfig.from_yaml(yaml_file)
+            tasks[task_config.name] = task_config
+
+        return tasks
+
+    def _is_duplicate(self, task_config: PulseConfig) -> bool:
+        all_tasks = self.tasks.values()
+        return any(task_config == task for task in all_tasks)
+
+    @property
+    def keys(self) -> list[str]:
+        return list(self.tasks.keys())
